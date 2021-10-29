fc18de2a2c38710662f5e05b11df275e3a7f578e
Ascendus <ascendus.dev@gmail.com>
2021-10-29 13:37:24 +1300

    Testing CLI v2

--- push.js
+++ push.js
@@ -2,7 +2,7 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 async function push() {
-    const { stdout, stderr } =await exec("tsc && git add . && git commit -m \"Testing CLI v2\" && git push origin main");
+    const { stdout, stderr } = await exec("tsc && git add . && git commit -m \"Testing CLI v2\" && git push origin main");
     console.log("stdout:", stdout);
     console.error("stderr:", stderr);
 };
