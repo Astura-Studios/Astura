4a545ded72cf88937c82d010ad8f8eb6bf825a65
Ascendus <ascendus.dev@gmail.com>
2021-10-29 13:39:12 +1300

    Updated push file

--- push.js
+++ push.js
@@ -2,9 +2,9 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 async function push() {
-    const { stdout, stderr } = await exec("tsc && git add . && git commit -m \"Testing CLI v2\" && git push origin main");
-    console.log("stdout:", stdout);
-    console.error("stderr:", stderr);
+    const { stdout, stderr } = await exec("tsc && git add . && git commit -m \"Updated push file\" && git push origin main");
+    console.log(stdout);
+    console.error(stderr);
 };
 
 push();
